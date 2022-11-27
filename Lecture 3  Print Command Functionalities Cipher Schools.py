b4a075df2563f06880c4863dabaef1bb13f00fe3
Ruvaidahmad123 <118987852+Ruvaidahmad123@users.noreply.github.com>
2022-11-27 17:30:54 +0530

    Add files via upload

---
+++ Lecture 3  Print Command Functionalities Cipher Schools.py
@@ -0,0 +1,14 @@
+print("Ruvaid Ahmad")
+print(1)
+#seperator
+print(1,2,3,4,5,sep="-")
+print(1,2,3,"ruvaid",3+4j,3.4,sep=",")
+
+
+#use of end while printing
+print("a",end=")")
+print("b")
+print("c")
+
+print("Hello",end=",")
+print("world")
